79aa36a17b1e89c7176583c6478987ab9f9e8bf7
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-05 02:03:44 +0100

    adding all files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 '''working on personal data'''
 import re
 from typing import Union, List
+import logging
 
 
 def filter_datum(fields: List[Union[str, str]], redaction: str,
@@ -13,3 +14,22 @@ def filter_datum(fields: List[Union[str, str]], redaction: str,
             if y in sp_mes[x]:
                 sp_mes[x] = re.sub(r'=\S*', f"={redaction}", sp_mes[x])
     return separator.join(sp_mes)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Here goes nothing'''
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
