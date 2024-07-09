64876f795f56dbee73e2974e961ae98ff3a2fc62
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-10 00:12:17 +0100

    adding all files

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,14 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     '''this is function inherit from Auth'''
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        '''extracting authorization header'''
+        if authorization_header is None:
+            return
+        if not isinstance(authorization_header, str):
+            return
+        if "Basic " not in authorization_header:
+            return
+        return authorization_header.split("Basic ")[1]
