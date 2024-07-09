ae73678750bba4386f11e14cabf8eb8c41fd2527
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-09 16:27:11 +0100

    adding all files

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """deals with everything auth
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False for now
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Returns None
+        """
+        return None
