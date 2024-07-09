d6186654e7849e23479c0e754a148056e037ef1f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-09 16:40:28 +0100

    adding all files

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,17 +7,20 @@ class Auth:
     """deals with everything auth
     """
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool | None:
         """returns False for now
         """
         return False
 
-    def authorization_header(self, request: flask.request = None) -> str:
+    def authorization_header(self,
+                             request: flask.request = None) -> str | None:
         """ returns None
         """
         return None
 
-    def current_user(self, request: flask.request = None) -> TypeVar('User'):
+    def current_user(self,
+                     request: flask.request = None) -> TypeVar('User') | None:
         """Returns None
         """
         return None
