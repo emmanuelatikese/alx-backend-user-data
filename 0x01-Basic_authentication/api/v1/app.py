6633574d1320b66a1a5451e818ff1f0a9aadbf1d
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-09 23:01:27 +0100

    adding all files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -10,14 +10,15 @@ import os
 from typing import Optional
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 if getenv("AUTH_TYPE") == 'auth':
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' This file contains function Basic Auth'''
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    '''this is function inherit from Auth'''
