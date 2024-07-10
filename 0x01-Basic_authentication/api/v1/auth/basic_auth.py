c26eb79d69b0d36d38e0fb7f7c008d5c0bfa90fa
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-10 02:24:45 +0100

    adding all files

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -44,7 +44,7 @@ class BasicAuth(Auth):
             return None, None
         if ':' not in decoded_base64_authorization_header:
             return None, None
-        _split = decoded_base64_authorization_header.split(':')
+        _split = decoded_base64_authorization_header.split(':', 1)
         return _split[0], _split[1]
 
     def user_object_from_credentials(self,
