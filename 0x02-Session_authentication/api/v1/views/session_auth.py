627d5cb0167b03451bd78d2a698c3382382c8887
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-13 10:12:18 +0100

    adding

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -17,12 +17,15 @@ def session_auth():
         return jsonify({"error": "password missing"}), 400
     try:
         new_users = User.search({'email': email})
+        if not new_users:
+            return jsonify({"error": "no user found for this email"})
         for user in new_users:
             if user.is_valid_password(password):
                 from api.v1.app import auth
-                sess_id = auth.create_session(user.id)
+                act_user = user
+                sess_id = auth.create_session(act_user.id)
                 sess_name = getenv('SESSION_NAME')
-                res = jsonify(user.to_json())
+                res = jsonify(act_user.to_json())
                 res.set_cookie(sess_name, sess_id)
                 return res
         return jsonify({"error": "wrong password"})
