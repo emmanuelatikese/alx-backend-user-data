d6c50a37b32b6db06e64c1c507d6ee4973ef51fe
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-16 22:50:28 +0100

    adding files

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +39,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        '''finding user by email'''
+        if not kwargs:
+            raise InvalidRequestError
+        for k in kwargs.keys():
+            if not hasattr(User, k):
+                raise InvalidRequestError
+        filtered_user = self._session.query(User).filter_by(**kwargs).first()
+        if not filtered_user:
+            raise NoResultFound
+        return filtered_user
