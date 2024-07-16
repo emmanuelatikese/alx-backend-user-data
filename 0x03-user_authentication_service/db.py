84eea665b1c44a96cd4b14271d4060274ed04d97
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-16 22:12:04 +0100

    adding files

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,4 +36,4 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-        return User
+        return user
