92a1265ff3ed3b723e74bcdca72215d20b05cf18
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-17 23:39:51 +0100

    adding files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 '''settting up flask
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
@@ -11,6 +14,23 @@ def index():
     '''return json
     '''
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    '''registration purposes
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        return
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user:
+            return jsonify({"email": "<registered email>",
+                            "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
