c3af59571ac3208f318f6b0c805eca1a66ab719c
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-17 22:09:25 +0100

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 '''File contains function for hashing pwd'''
+from db import DB
 import bcrypt
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -8,3 +10,21 @@ def _hash_password(password: str) -> bytes:
     """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """for registration of user
+        """
+        fd_user = self._db._session.query(User).filter_by(email=email).first()
+        if fd_user:
+            raise ValueError("User <user's email> already exists")
+        hash_pwd = _hash_password(password)
+        new_user = self._db.add_user(email=email, hashed_password=hash_pwd)
+        return new_user
