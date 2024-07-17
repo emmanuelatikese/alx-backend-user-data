937a7bf11f7889de768de49f2d27c3258867b86b
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-17 23:48:19 +0100

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,16 @@ class Auth:
             hash_pwd = _hash_password(password)
             new_user = self._db.add_user(email=email, hashed_password=hash_pwd)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''validating password
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode('utf-8'),
+                                       user.hashed_password):
+                return True
+            else:
+                return False
+        except Exception:
+            return False
