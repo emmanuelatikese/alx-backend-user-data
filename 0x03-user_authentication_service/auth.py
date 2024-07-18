f6a3d122c1cd1dd242f3d38576751dbea44eb492
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-18 01:21:46 +0100

    adding files

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,13 +76,12 @@ class Auth:
         except Exception:
             return
 
-    def destory_session(self, user_id: int) -> None:
+    def destroy_session(self, user_id: int) -> None:
         '''destroy session
         '''
         try:
             user = self._db.find_user_by(user_id=user_id)
             if user:
                 self._db.update_user(user_id, session_id=None)
-            return
-        except NoResultFound:
-            return
+        except Exception:
+            return None
