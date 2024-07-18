1362c68b9f0477d546a1ac1a44905009f0bb2501
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-18 22:35:49 +0100

    adding files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,20 @@ def profile() -> str:
     abort(403)
 
 
+@app.route('/reset_password', strict_slashes=False)
+def get_reset_password_token():
+    '''reset password token
+    '''
+    email = request.form.get('email')
+    if not email:
+        return
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email,
+                        "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
