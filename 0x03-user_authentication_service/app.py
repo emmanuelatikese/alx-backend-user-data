0e17a97c0369a8f17c7a241fb03a20d9345fc1da
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-07-18 01:01:10 +0100

    adding files

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''settting up flask
 '''
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -27,11 +27,27 @@ def users():
     try:
         new_user = AUTH.register_user(email, password)
         if new_user:
-            return jsonify({"email": "<registered email>",
+            return jsonify({"email": email,
                             "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    '''all logins
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        abort(401)
+    lg_user = AUTH.valid_login(email, password)
+    if lg_user:
+        sess = AUTH.create_session(email)
+        res = make_response(jsonify({"email": email,
+                                     "message": "logged in"}))
+        res.set_cookie('session_id', sess)
+        return res
+    return abort(401)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
